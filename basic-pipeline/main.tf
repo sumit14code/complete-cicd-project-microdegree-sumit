fe7b39b4db90ed32cd86b3ec6fc4c84a6636cac9
Manoj-K <k_manoj@apple.com>
2024-09-09 13:48:46 +0530

    basic config

---
+++ basic-pipeline/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "instance-1" {
+    ami = "ami-0e86e20dae9224db8"
+    instance_type = "t2.micro"
+    count = "1"
+    security_groups = ["default"]
+    key_name = "project"
+    user_data = file("server-script.sh")
+    tags = {
+      Name = "versioning"
+    }
+}
+
+
